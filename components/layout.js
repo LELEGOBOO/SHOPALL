4f3c34ae5ad311aa48287843bf3a2c101cec716e
LELEGOBOO <lelegoboo@gmail.com>
2026-04-02 16:21:17 +0300

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -1,1 +1,14 @@
+import Navbar from './Navbar';
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <Navbar />
+      <main className="pt-16 min-h-screen">{children}</main>
+      <footer className="p-10 bg-black text-white text-center text-sm">
+        © 2026 GLAMM2 | All Rights Reserved
+      </footer>
+    </>
+  );
+}
 
