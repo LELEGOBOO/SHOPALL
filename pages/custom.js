9a8998e612f603ffb3f61e4057520bc5dba07540
LELEGOBOO <lelegoboo@gmail.com>
2026-04-02 20:15:57 +0300

    Add Custom component for design uploads

--- pages/custom.js
+++ pages/custom.js
@@ -1,1 +1,12 @@
+export default function Custom() {
+  return (
+    <div className="p-10 text-center">
+      <h1 className="text-3xl font-bold">Custom Designs</h1>
+      <div className="mt-10 border-2 border-dashed border-gray-300 p-20">
+        <p>Upload your design to start</p>
+        <input type="file" className="mt-4" />
+      </div>
+    </div>
+  );
+}
 
